77adc061203a6d283df6c172586da376683d8677
ToobaKS <tksheikh17@gmail.com>
2021-10-22 11:49:53 -0400

    Started creating the board by creating properties

--- src/Board.java
+++ src/Board.java
@@ -2,4 +2,15 @@ import java.util.ArrayList;
 
 public class Board {
 
+    private ArrayList<Property> properties;
+
+    public Board() {
+        this.properties = new ArrayList<>();
+
+        createProperties();
+    }
+
+    private void createProperties(){
+
+    }
 }
