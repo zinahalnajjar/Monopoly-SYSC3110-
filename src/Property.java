4e3eb88acffa280534391f43222b78cab8c42b37
ToobaKS <tksheikh17@gmail.com>
2021-10-22 11:43:40 -0400

    Added variables in property

--- src/Property.java
+++ src/Property.java
@@ -1,4 +1,18 @@
 public class Property {
+
     private String propertyName;
+    private String color;
+    private Player owner;
+    private int rent;
+    private int cost;
+
+    public Property(String propertyName, String color, int rent, int cost) {
+        this.propertyName = propertyName;
+        this.color = color;
+        this.rent = rent;
+        this.cost = cost;
+        this.owner = null;
+    }
+
 
 }
