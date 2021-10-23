dd0e089133ceb64bcf4f07e8345beeee89da9899
ToobaKS <tksheikh17@gmail.com>
2021-10-23 09:26:47 -0400

    Added some basic structure for the main class

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Game {
+
+    private Board board;
+    private ArrayList<Player> players;
+
+    public Game() {
+        board = new Board();
+        players = new ArrayList<Player>();
+        initPlayers();
+        run();
+    }
+
+    private void initPlayers() {
+    }
+
+    private void run() {
+    }
+
+    public void buy(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/Property.java
+++ src/Property.java
@@ -1,3 +1,6 @@
+/**
+ *
+ */
 public class Property {
 
     private String propertyName;
@@ -6,6 +9,12 @@ public class Property {
     private int rent;
     private int cost;
 
+    /**
+     * @param propertyName
+     * @param color
+     * @param rent
+     * @param cost
+     */
     public Property(String propertyName, String color, int rent, int cost) {
         this.propertyName = propertyName;
         this.color = color;
@@ -14,42 +23,72 @@ public class Property {
         this.owner = null;
     }
 
+    /**
+     * @return
+     */
     public String getPropertyName() {
         return propertyName;
     }
 
+    /**
+     * @param propertyName
+     */
     public void setPropertyName(String propertyName) {
         this.propertyName = propertyName;
     }
 
+    /**
+     * @return
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * @param color
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * @return
+     */
     public Player getOwner() {
         return owner;
     }
 
+    /**
+     * @param owner
+     */
     public void setOwner(Player owner) {
         this.owner = owner;
     }
 
+    /**
+     * @return
+     */
     public int getRent() {
         return rent;
     }
 
+    /**
+     * @param rent
+     */
     public void setRent(int rent) {
         this.rent = rent;
     }
 
+    /**
+     * @return
+     */
     public int getCost() {
         return cost;
     }
 
+    /**
+     * @param cost
+     */
     public void setCost(int cost) {
         this.cost = cost;
     }
