ffeb6126ccd95f490155d71fc738ffe25b99481d
ToobaKS <tksheikh17@gmail.com>
2021-10-23 09:38:07 -0400

    initialized the players, set up game loop, added some methods for basic functionality

--- src/Game.java
+++ src/Game.java
@@ -4,21 +4,47 @@ public class Game {
 
     private Board board;
     private ArrayList<Player> players;
+    private boolean gameOver;
+    private Dice dice;
+    private Player currentPlayer;
 
     public Game() {
         board = new Board();
         players = new ArrayList<Player>();
+        gameOver = false;
+
         initPlayers();
         run();
+
     }
 
     private void initPlayers() {
+        for(int i = 1; i < 5; i++){
+            players.add(new Player(i, 1500));
+        }
+
+        currentPlayer = players.get(0);
     }
 
     private void run() {
+        while(gameOver != true){
+
+        }
     }
 
     public void buy(){
+
+    }
+
+    public void sell(){
+
+    }
+
+    public void pass(){
+
+    }
+
+    public void CheckWin(){
 
     }
 
@@ -26,3 +52,4 @@ public class Game {
 
     }
 }
+
