d28cb2f2c02044ba68dd0f337f847d3bbec83348
Kareem Abdo <93018936+KnightAlfa@users.noreply.github.com>
2021-10-24 10:30:27 -0400

    added getValidLocation

--- src/Board.java
+++ src/Board.java
@@ -73,4 +73,17 @@ public class Board {
     public ArrayList<Property> propertiesList(){
         return properties;
     }
+
+    public int getValidLocation(int newLocation) {
+        if (getProperty(newLocation) == null) {
+            return 0;
+        } else {
+            return newLocation;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Board [properties Count=" + properties.size() + "]";
+    }
 }
