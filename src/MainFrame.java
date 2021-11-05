55cdb6c1cc9fbc8be6af4a7b4c50d922a3dd2b2a
ToobaKS <tksheikh17@gmail.com>
2021-11-05 15:57:32 -0400

    Started main frame

---
+++ src/MainFrame.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame implements MonopolyView {
+
+    JPanel boardPanel; //For the grid of properties
+    JPanel sidePanel; // For the buttons and player info
+
+    JButton pass;
+    JButton roll;
+    JButton playerInfo;
+
+    public MainFrame(){
+        super("Monopoly!!");
+
+       this.setLayout(new BorderLayout());
+
+        boardPanel = new JPanel();
+        sidePanel = new JPanel();
+        sidePanel.setLayout(new GridLayout(6,1));
+
+        this.add(boardPanel);
+        this.add(sidePanel);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 500);
+
+        boardPanel.setSize(500,500);
+        sidePanel.setSize(300, 500);
+
+        boardPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        sidePanel.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        pass = new JButton("Pass");
+        roll = new JButton("Roll");
+        playerInfo = new JButton("Player Info");
+
+        addButton(pass);
+        addButton(roll);
+        addButton(playerInfo);
+
+        this.setVisible(true);
+    }
+
+    public void addButton(JButton button){
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        sidePanel.add(button);
+    }
+
+    public static void main(String[] args){
+        new MainFrame();
+    }
+}
+
---
+++ src/StartFrame.java
@@ -0,0 +1,2 @@
+public class StartFrame {
+}
