e4b0d6f762c037d425df8ae830d0909fdb45a6d7
ToobaKS <tksheikh17@gmail.com>
2021-11-06 14:24:34 -0400

    started a basic controller class

--- src/MonopolyController.java
+++ src/MonopolyController.java
@@ -1,4 +1,13 @@
-public class MonopolyController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class MonopolyController implements ActionListener {
 
+    public MonopolyController(){
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
